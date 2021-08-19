bb13993d29e0cd4d7be6bc31b4b00495bf89b63f
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-08-18 23:10:12 -0700

    Implement countLetters in Lotide

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+const assertArraysEqual = function(actualArr, expectedArr) {
+  if (eqArrays(actualArr, expectedArr)) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actualArr} === ${expectedArr}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actualArr} !== ${expectedArr}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+// need to check both var are array
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+
+        return false;
+      }
+    }
+  } else {
+
+    return false;
+  }
+
+  return true;
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (!results[sentence[i]]) {
+      results[sentence[i]] = [];
+    }
+    results[sentence[i]].push(i);
+  }
+
+  return results;
+};
+
+const testCase = letterPositions('lighthouse in the house');
+
+assertArraysEqual(testCase.l, [0]);
+assertArraysEqual(testCase.i, [1, 11]);
+assertArraysEqual(testCase.e, [9, 16, 22]);
