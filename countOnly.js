130d3c504647cdc93bc78f1d0c2c9872593ab843
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-08-18 21:56:23 -0700

    Implement countOnly in Lotide

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let result = {};
+
+  for (let item of allItems) {
+    for (let cmprKey in itemsToCount) {
+      if ((item === cmprKey) && itemsToCount[cmprKey]) {
+        //if (result[item]) {
+        //if (result.hasOwnProperty(item)) {
+        if (Object.prototype.hasOwnProperty.call(result, item)) {
+          result[item]++;
+        } else {
+          result[item] = 1;
+        }
+      }
+    }
+  }
+
+  return result;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
