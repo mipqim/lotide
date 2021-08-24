3528558d0638e2850543f1c00cd41b0fa1bbb6fb
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-08-24 16:26:01 -0700

    Refactor middle.js and tail.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,21 +1,23 @@
-
-
-
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle.js');
 
 describe("#middle", () => {
-  it("returns []] for [1]", () => {
-    assert.strictEqual(middle([1]), []);
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
   });
-  it("returns [] for [1, 2, 3]", () => {
-    assert.strictEqual(middle([1, 2, 3]), '2'); 
+  it("returns [] for [1,2]", () => {
+    assert.deepEqual(middle([1,2]), []);
   });  
+  it("returns [] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]); 
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
+  });
+  it("returns [2, 3] for [1, 2, 3, 4, 5, 6, 7, 8]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]); 
+  });
+  it("returns ['middle'] for [1, 2, 3, 4, 'middle', 6, 7, 8, 9]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 'middle', 6, 7, 8, 9]), ['middle']); 
+  });       
 });
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
-assertArraysEqual(middle([1,2,3,4,'middle',6,7,8,9]), ['middle']);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,44 @@
-const assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
 const tail = require('../tail.js');
 
+describe("#tail", () => {
 
-const testResult1 = tail([5, 6, 7]);
-assertEqual(testResult1.length, 2);
-assertEqual(testResult1[0], 6);
-assertEqual(testResult1[1], 7);
-console.log('----');
+  const testResult1 = tail([5, 6, 7]);
+  it("returns 2 for tail([5, 6, 7]).length", () => {
+    assert.strictEqual(testResult1.length, 2);
+  });
+  it("returns 6 for tail([5, 6, 7])[0]]", () => {
+    assert.strictEqual(testResult1[0], 6);
+  });
+  it("returns 7 for tail([5, 6, 7])[1]]", () => {
+    assert.strictEqual(testResult1[1], 7);
+  });  
 
-const testResult2 = tail(["Hello", "Lighthouse", "Labs"]);
+  const testResult2 = tail(["Hello", "Lighthouse", "Labs"]);
+  it("returns 2 for tail(['Hello', 'Lighthouse', 'Labs']).length", () => {
+    assert.strictEqual(testResult2.length, 2);
+  });
+  it("returns 'Lighthouse' for tail(['Hello', 'Lighthouse', 'Labs'])[0]]", () => {
+    assert.strictEqual(testResult2[0], 'Lighthouse');
+  });
+  it("returns 'Labs' for tail(['Hello', 'Lighthouse', 'Labs'])[1]]", () => {
+    assert.strictEqual(testResult2[1], 'Labs');
+  });  
+
+
+  it("returns 0 for tail(['one']).length", () => {
+    assert.strictEqual(tail(['one']).length, 0);
+  }); 
+
+  it("returns 0 for tail([]).length", () => {
+    assert.strictEqual(tail([]).length, 0);
+  }); 
+
+});
+
+/*
+
+const testResult2 = tail(['Hello', 'Lighthouse', 'Labs']);
 assertEqual(testResult2.length, 2);
 assertEqual(testResult2[0], "Lighthouse");
 assertEqual(testResult2[1], "Labs");
@@ -19,4 +49,4 @@ assertEqual(testResult3.length, 0);
 console.log('----');
 
 const testResult4 = tail([]);
-assertEqual(testResult4.length, 0);
+assertEqual(testResult4.length, 0);*/
