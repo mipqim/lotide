b6e2c9d14b184ef924e7bd93f16ad99278fbf578
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-08-24 04:22:43 -0700

    Refactor head.js with Chai and Mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,27 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head("ddd1"), "d");
-assertEqual(head(100), undefined);
-assertEqual(head([7]), 7);
-assertEqual(head([]), undefined);
-assertEqual(head(), undefined);
-assertEqual(head(null), undefined);
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns Hello for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });   
+  it("returns 7 for [7]", () => {
+    assert.strictEqual(head([7]), 7);
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined); 
+  });  
+  it("returns undefined for *no arg*", () => {
+    assert.strictEqual(head(), undefined);
+  });
+  it("returns undefined for null", () => {
+    assert.strictEqual(head(null), undefined); 
+  });    
+});
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,17 @@
+
+
+
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle.js');
+
+describe("#middle", () => {
+  it("returns []] for [1]", () => {
+    assert.strictEqual(middle([1]), []);
+  });
+  it("returns [] for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), '2'); 
+  });  
+});
 
 assertArraysEqual(middle([1]), []);
 assertArraysEqual(middle([1,2]), []);
